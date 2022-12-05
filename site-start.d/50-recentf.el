840fed6d984ba82a6e638adbcf1027c1668adf4b
Adam Chyła <adam@chyla.org>
2022-12-05 23:20:31 +0100

    change recentf max items

--- site-start.d/50-recentf.el
+++ site-start.d/50-recentf.el
@@ -1,4 +1,7 @@
 (recentf-mode 1)
 
-; call `recentf-save-list` periodically
+(setq recentf-max-menu-items 25)
+(setq recentf-max-saved-items 50)
+
+;; call `recentf-save-list` periodically
 (run-at-time nil (* 5 60) 'recentf-save-list)
