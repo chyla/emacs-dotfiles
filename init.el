f6d9bcd5deb5353c4ae284e1bbb0580e60f5e521
Adam Chyła <adam@chyla.org>
2022-12-05 22:46:10 +0100

    zenburn theme

--- init.el
+++ init.el
@@ -7,9 +7,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("5f08bcb2aeea99909bf2508b9926925e581068b14053306b73ad70cbfa723c9f" "834530aef9fbd26a37e87ab77397511db4451f5c0b0d02881fbc5847ef436f83" default))
+   '("2dc03dfb67fbcb7d9c487522c29b7582da20766c9998aaad5e5b63b5c27eec3f" "5f08bcb2aeea99909bf2508b9926925e581068b14053306b73ad70cbfa723c9f" "834530aef9fbd26a37e87ab77397511db4451f5c0b0d02881fbc5847ef436f83" default))
  '(package-selected-packages
-   '(flycheck magit markdown-mode robot-mode ace-window yasnippet-snippets yasnippet company chyla-theme diff-hl)))
+   '(zenburn-theme flycheck magit markdown-mode robot-mode ace-window yasnippet-snippets yasnippet company chyla-theme diff-hl)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
