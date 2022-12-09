1871ddbb13cd02a257298eea70f45d7236f3d2ab
Adam Chyła <adam@chyla.org>
2022-12-09 07:56:56 +0100

    update magit, company

--- elpa/company-20221206.2122/company-pkg.el
+++ elpa/company-20221206.2122/company-pkg.el
@@ -1,6 +1,6 @@
-(define-package "company" "20221130.1326" "Modular text completion framework"
+(define-package "company" "20221206.2122" "Modular text completion framework"
   '((emacs "25.1"))
-  :commit "00a739f9684c68c8445b04f33fb8536a07ae34bd" :authors
+  :commit "6884e3ad717419b4a64a5fab08c8cb9bd20a0b27" :authors
   '(("Nikolaj Schumacher"))
   :maintainer
   '("Dmitry Gutov" . "dgutov@yandex.ru")
--- elpa/magit-20221208.1848/magit-pkg.el
+++ elpa/magit-20221208.1848/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20221127.2227" "A Git porcelain inside Emacs."
+(define-package "magit" "20221208.1848" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (compat "28.1.1.2")
     (dash "20210826")
@@ -6,7 +6,7 @@
     (magit-section "20221127")
     (transient "20220325")
     (with-editor "20220318"))
-  :commit "3cf308c1c4cdb9404e3d3c6f9935ad9f628311d9" :authors
+  :commit "99d9b3008adf72a0c2cdb7df70b5ae03ea9271c8" :authors
   '(("Marius Vollmer" . "marius.vollmer@gmail.com")
     ("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
