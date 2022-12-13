7b4945dcf6d63322cecccd983c2b71706f238b08
Adam Chyła <adam@chyla.org>
2022-12-13 11:01:08 +0100

    use kill-file-path from melpa

---
+++ elpa/kill-file-path-20221205.1119/kill-file-path-autoloads.el
@@ -0,0 +1,23 @@
+;;; kill-file-path-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "kill-file-path" "kill-file-path.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from kill-file-path.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "kill-file-path" '("kill-file-path")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; kill-file-path-autoloads.el ends here
