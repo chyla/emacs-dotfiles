5efe56eb075034dd894ce57de2603325f0eb6d7b
Adam Chyła <adam@chyla.org>
2022-12-21 21:10:56 +0100

    change ttcn3 mode name to autoload

--- site-start.d/50-ttcn-mode.el
+++ site-start.d/50-ttcn-mode.el
@@ -1,10 +1,7 @@
-; TTCN-3 Support
-; https://github.com/dholm/ttcn-el
-
 (setq ttcn-extension-directory (concat user-emacs-directory "/extensions/ttcn-el"))
 
 (add-to-list 'load-path ttcn-extension-directory)
 (require 'ttcn3)
 
-(autoload 'ttcn-3-mode "ttcn-3-mode" "TTCN3 editing mode." t)
-(setq auto-mode-alist (cons '("\\.ttcn3?" . ttcn-3-mode) auto-mode-alist))
+(autoload 'ttcn3-mode "ttcn3-mode" "TTCN3 editing mode." t)
+(setq auto-mode-alist (cons '("\\.ttcn3?" . ttcn3-mode) auto-mode-alist))
