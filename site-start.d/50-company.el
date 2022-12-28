2de7135ae8512f2d3d5ad6bcbb2905efbfa090dd
Adam Chyła <adam@chyla.org>
2022-12-28 16:55:52 +0100

    add company-dabbrev backend

--- site-start.d/50-company.el
+++ site-start.d/50-company.el
@@ -1,7 +1,12 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
-(setq company-backends '( ( company-keywords company-yasnippet company-dabbrev-code company-etags
-                            )))
+(setq company-backends '( (
+    company-keywords
+    company-yasnippet
+    company-dabbrev
+    company-dabbrev-code
+    company-etags
+)))
 
 (setq company-minimum-prefix-length 1)
 (setq company-idle-delay 0.2)
