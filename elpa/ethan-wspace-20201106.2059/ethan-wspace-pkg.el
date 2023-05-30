1183c0008bf386378484484dc2fdd92de31fb190
Adam Chyła <adam@chyla.org>
2023-05-30 17:16:01 +0200

    remove ethan-wspace
    Removing due to bug:
    Clears colon inside comment when line contains
    colon as last character.

--- elpa/ethan-wspace-20201106.2059/ethan-wspace-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from ethan-wspace-20201106.2059/ethan-wspace.el  -*- no-byte-compile: t -*-
-(define-package "ethan-wspace" "20201106.2059" "whitespace customizations for emacs" 'nil :commit "035c7d698c99e3891a522d6e6f8fde23c6267c15" :authors '(("Ethan Glasser-Camp" . "ethan@betacantrips.com")) :maintainer '("Ethan Glasser-Camp" . "ethan@betacantrips.com") :keywords '("whitespace" "tab" "newline" "trailing" "clean"))
--- site-start.d/50-ethan-wspace.el
+++
@@ -1,4 +0,0 @@
-;; let ethan-wspace handle the final new line
-(setq mode-require-final-newline nil)
-
-(global-ethan-wspace-mode 1)
