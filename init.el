aa1c8ad3d8a14c927a107d510bcc8eb30f2d9164
Adam Chyła <adam@chyla.org>
2023-07-09 14:47:05 +0200

    Revert "add material-theme"
    This reverts commit 047b36c487375d266741ebd0d799e8ed94319484.

--- init.el
+++ init.el
@@ -7,10 +7,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "f74e8d46790f3e07fbb4a2c5dafe2ade0d8f5abc9c203cd1c29c7d5110a85230" "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" "e47dcceeea2fb5866d8c955fb6d9db94f7580ceff4986ca8d1417ae07e4a64a1" "2dc03dfb67fbcb7d9c487522c29b7582da20766c9998aaad5e5b63b5c27eec3f" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "5f08bcb2aeea99909bf2508b9926925e581068b14053306b73ad70cbfa723c9f" "834530aef9fbd26a37e87ab77397511db4451f5c0b0d02881fbc5847ef436f83" default))
+   '("e47dcceeea2fb5866d8c955fb6d9db94f7580ceff4986ca8d1417ae07e4a64a1" "2dc03dfb67fbcb7d9c487522c29b7582da20766c9998aaad5e5b63b5c27eec3f" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "5f08bcb2aeea99909bf2508b9926925e581068b14053306b73ad70cbfa723c9f" "834530aef9fbd26a37e87ab77397511db4451f5c0b0d02881fbc5847ef436f83" default))
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(material-theme ws-butler kill-file-path yaml-mode spacemacs-theme zenburn-theme flycheck magit markdown-mode robot-mode ace-window yasnippet-snippets yasnippet company chyla-theme diff-hl)))
+   '(ws-butler kill-file-path yaml-mode spacemacs-theme zenburn-theme flycheck magit markdown-mode robot-mode ace-window yasnippet-snippets yasnippet company chyla-theme diff-hl)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
