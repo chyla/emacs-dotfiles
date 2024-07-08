017e4e789a1e21c379a4feb6581e717070b9b3ae
Adam Chyła <adam@chyla.org>
2024-07-08 08:42:31 +0200

    install chyla-dark-theme

---
+++ elpa/chyla-dark-theme-20240707.2225/chyla-dark-theme-autoloads.el
@@ -0,0 +1,29 @@
+;;; chyla-dark-theme-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "chyla-dark-theme" "chyla-dark-theme.el" (0
+;;;;;;  0 0 0))
+;;; Generated autoloads from chyla-dark-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(register-definition-prefixes "chyla-dark-theme" '("chyla-dark"))
+
+;;;***
+
+;;;### (autoloads nil nil ("chyla-dark-theme-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; chyla-dark-theme-autoloads.el ends here
---
+++ elpa/chyla-dark-theme-20240707.2225/chyla-dark-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "chyla-dark-theme" "20240707.2225" "Chyla.org - dark green color theme"
+  '((emacs "24.1"))
+  :commit "d115fcbf36964b7660ec87773125970a32dd31cf" :authors
+  '(("Adam Chyła https://chyla.org/" . "adam@chyla.org"))
+  :maintainers
+  '(("Adam Chyła https://chyla.org/" . "adam@chyla.org"))
+  :maintainer
+  '("Adam Chyła https://chyla.org/" . "adam@chyla.org")
+  :url "https://github.com/chyla/ChylaDarkThemeForEmacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
