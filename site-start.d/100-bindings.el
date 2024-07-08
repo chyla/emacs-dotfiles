3432b3fbcd36805b8d88325458595c24b5bad350
Adam Chyła <adam@chyla.org>
2024-07-08 08:51:41 +0200

    add light, dark bindings to load theme

--- site-start.d/100-bindings.el
+++ site-start.d/100-bindings.el
@@ -2,3 +2,13 @@
   "Call recentf-open-files."
   (interactive)
   (recentf-open-files))
+
+(defun light ()
+  "Load light chyla.org theme."
+  (interactive)
+  (load-theme 'chyla))
+
+(defun dark ()
+  "Load dark chyla.org theme."
+  (interactive)
+  (load-theme 'chyla-dark))
