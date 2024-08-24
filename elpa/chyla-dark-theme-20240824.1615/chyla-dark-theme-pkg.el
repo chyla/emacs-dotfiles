33b6d534f39d27f6b34b2fc563812b4b705028d0
Adam Chyła <adam@chyla.org>
2024-08-25 00:53:25 +0200

    packages update

---
+++ elpa/chyla-dark-theme-20240824.1615/chyla-dark-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "chyla-dark-theme" "20240824.1615" "Chyla.org - dark green color theme"
+  '((emacs "24.1"))
+  :commit "274ff01146e265f773478b16a59483b638b986f8" :authors
+  '(("Adam Chyła https://chyla.org/" . "adam@chyla.org"))
+  :maintainers
+  '(("Adam Chyła https://chyla.org/" . "adam@chyla.org"))
+  :maintainer
+  '("Adam Chyła https://chyla.org/" . "adam@chyla.org")
+  :url "https://github.com/chyla/ChylaDarkThemeForEmacs")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
